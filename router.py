d471cc0af946b927701041815ab402a2908bc2e4
Samuel Alba <sam.alba@gmail.com>
2011-06-25 16:44:08 -0700

    Removed unused import

--- router.py
+++ router.py
@@ -17,9 +17,8 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-import os
 import re
-from webob import Request, Response, exc
+from webob import exc
 
 
 def pair(iterable):
